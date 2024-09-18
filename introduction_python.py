5311e0066a470cb03ac515ddf54a3d40e9fa5ab2
Marcos Coelho <mviniciusca@gmail.com>
2024-09-18 18:58:36 -0300

    wip

--- introduction_python.py
+++ introduction_python.py
@@ -1,1 +1,26 @@
+# Hello World
 print("Hello World!")
+
+## Variables
+firstName = 'Marcos'
+lastName = 'Coelho'
+fullName = firstName + ' ' + lastName
+
+print(fullName)
+
+## Types
+name = 'John Doe' # string
+age = 35 # integer
+point = 4.76 # float
+approved = False  # boolean
+
+print(type(name)) # => <class 'str'> => string
+print(type(age)) # => <class 'int'> => int
+print(type(point)) # => <class 'float'> => float
+print(type(approved)) # => <class 'bool'> =>  bool
+
+
+
+
+
+
