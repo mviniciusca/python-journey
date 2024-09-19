424f34273cd6f747bcbd60ffcfb212066d71bd1b
Marcos Coelho <mviniciusca@gmail.com>
2024-09-19 14:06:44 -0300

    wip

--- projects/exercise_1.py
+++ projects/exercise_1.py
@@ -1,1 +1,16 @@
 ## Media
+media1 = float(input("Note 1: "))
+media2 = float(input("Note 2: "))
+media3 = float(input("Note 3: "))
+media4 = float(input("Note 4: "))
+
+media = (media1 + media2 + media3 + media4) / 4
+
+approved = 6
+
+if (media >= approved):
+    status = 'approved'
+else:
+    status = 'reproved'
+
+print(f"Your media is: {media} and your status is: {status}")
