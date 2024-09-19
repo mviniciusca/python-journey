de13ea98533b282a36b42ef868197b7ad1ed54d7
Marcos Coelho <mviniciusca@gmail.com>
2024-09-19 12:18:21 -0300

    wip

--- projects/exercise_1.py
+++ projects/exercise_1.py
@@ -1,8 +1,8 @@
-media1 = int(input("Note 1: "))
-media2 = int(input("Note 2: "))
-media3 = int(input("Note 3: "))
-media4 = int(input("Note 4: "))
-media = (media1 + media2 + media3 + media4)/4
+media1 = float(input("Note 1: "))
+media2 = float(input("Note 2: "))
+media3 = float(input("Note 3: "))
+media4 = float(input("Note 4: "))
+media = int((media1 + media2 + media3 + media4)/4)
 
 if(media >= 6):
     result = 'Approved'
