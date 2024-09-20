4b252325b01d1eac1e6aadfb722788803c8128c9
Marcos Coelho <mviniciusca@gmail.com>
2024-09-20 17:02:49 -0300

    wip

--- functions.py
+++ functions.py
@@ -8,3 +8,20 @@ def sum(a, b):
     result = a + b
     return result
 print(sum(a, b))
+
+def odd():
+    number = int(input("Enter a valid number: "))
+    if number % 2 == 0:
+        print("Even")       
+    else:
+       print("Odd")
+
+odd()
+
+'''
+Lambda fn
+'''
+
+sum = lambda a,b: a+b
+result = sum(3,8)
+print(result)
