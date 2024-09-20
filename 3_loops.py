6a7e352321447650f7b98d7d15565e4e476a1858
Marcos Coelho <mviniciusca@gmail.com>
2024-09-20 15:58:33 -0300

    wip

--- 3_loops.py
+++ 3_loops.py
@@ -1,5 +1,6 @@
 '''
 Loops in Python
+### For
 '''
 
 numbers = [1,2,3,4,5]
@@ -19,10 +20,28 @@ Example 3: Find the word
 
 search = input('Search for a product: ')
 products = ['ball', 't-shirt', 'paint', 'shoes']
+found = False
 for product in products:
     if(product == search):
         print(product)
+        found = True
         break
-'''
+if not found:
+    print("Product not Found")
 
-'''
+    
+    '''
+    ### While
+    '''
+    i = 1
+    while i < 10:
+        print(i)
+        i += 1
+
+    number = int(input("Enter a number (0 to stop)"))
+    while number != 0:
+        if number % 2 == 0:
+            print("Even")
+        else:
+            print("odd")
+        number = int(input("Enter a number (0 to stop)"))
