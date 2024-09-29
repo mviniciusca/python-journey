080e6773ef15da203d01498f0981c1aaaad957c8
Marcos Coelho <mviniciusca@gmail.com>
2024-09-29 19:27:35 -0300

    wip

--- portfolio/imc.py
+++ portfolio/imc.py
@@ -1,19 +1,20 @@
 '''
 IMC Calculator
 '''
+
 def imc():
+    ## Input
     height = float(input("Enter your height (m): "))
     weight = float(input("Enter your weight (Kg): "))
-
     ## Check if the value is not zero
     if height <= 0 or weight <= 0:
         print("Enter a valid value")
         height = float(input("Enter your height (m): "))
         weight = float(input("Enter your weight (Kg): "))
- 
+    ## Get the result
         result = weight / (height ** 2)
         result = round(result, 2)    
-
+    ## Classification
     if result <= 18.5:
             level = 'Under Normal'
     elif 18.6 <= result <= 24.9:
@@ -26,7 +27,8 @@ def imc():
             level = 'Obesity Level 2'
     else:
             level = 'Obesity Level 3' 
-
+    ## Final
     print(f'Your IMC is: {result} and it\'s mean that your level is {level} according to OMS')
 
+## Calling the fn
 imc()
