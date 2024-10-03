c161edbc005c47ec40eee6a8b6145b5fd23d54cc
Marcos Coelho <mviniciusca@gmail.com>
2024-10-03 16:45:08 -0300

    wip

--- portfolio/imc_ref.py
+++ portfolio/imc_ref.py
@@ -10,20 +10,11 @@ def validate(x,y):
         validate(x,y)
     else:
           return x,y
-    
-def imc():
-    y = float(input("Enter your heigh: "))
-    x = float(input("Enter your weight: "))
-
-    validate(x,y)
-    imc = round(imc_calc(x,y),2)
-    result = classification(imc)
-    print(f"Your IMC is: {imc} and your level is {result}")
 
 def imc_calc(x,y):
     result = x /(y ** 2)
     return result
- 
+    
 def classification(result):
     if result <= 18.5:
             level = 'Under Normal'
@@ -39,4 +30,12 @@ def classification(result):
             level = 'Obesity Level 3' 
     return level
 
+def imc():
+    y = float(input("Enter your heigh: "))
+    x = float(input("Enter your weight: "))
+
+    validate(x,y)
+    imc = round(imc_calc(x,y),2)
+    result = classification(imc)
+    print(f"Your IMC is: {imc} and your level is {result}") 
 imc()
